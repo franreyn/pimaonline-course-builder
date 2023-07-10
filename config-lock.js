5f1522113a58acab865fb903218ec4d4894af096
Francisco Reynoso <fjreynoso@pima.edu>
2023-07-10 13:10:44 -0700

    Moved export function to its own file

--- config-lock.js
+++ config-lock.js
@@ -4,8 +4,7 @@ import { handleEvents } from "./modules/handleEvents.js";
 import { defineCommands } from "./modules/defineCommands.js";
 import { setComponents } from "./modules/setComponents.js";
 import { handlePanels } from "./modules/handlePanels.js";
-// import { saveToLocal, saveWithDialog, openFromLocal } from './modules/fileOperations.js';
-// import { saveToLocal, loadFromLocal } from './modules/fileOperations.js';
+import { saveToLocal, openFromLocal, exportFile } from './modules/fileOperations.js';
 
 const editor = initEditor();
 setComponents(editor)
@@ -15,74 +14,11 @@ addCustomTypes(editor);
 handleEvents(editor);
 defineCommands(editor);
 
-// Buttons
-// const btnSave = document.getElementById("btn-save");
-// btnSave.addEventListener("click", () => saveToLocal(editor, false));
+const btnSaveAs = document.querySelector("#btn-save-as");
+btnSaveAs.addEventListener("click", () => saveToLocal(editor));
 
-const btnSaveAs = document.getElementById("btn-save-as");
-btnSaveAs.addEventListener('click', async () => {
-  const storedProjectData = await editor.store();
-  const blob = new Blob([JSON.stringify(storedProjectData)], {type: "application/json;charset=utf-8"});
-  saveAs(blob, "_grapes-project.json");
-});
+const btnOpen = document.querySelector("#btn-open");
+btnOpen.addEventListener("click", () => openFromLocal(editor));
 
-const btnOpen = document.getElementById("btn-open");
-btnOpen.addEventListener('click', () => {
-  const input = document.createElement('input');
-  input.type = 'file';
-  input.accept = 'application/json';
-  input.onchange = async () => {
-    const file = input.files[0];
-    const reader = new FileReader();
-    reader.onload = async () => {
-      const projectData = JSON.parse(reader.result);
-      await editor.load(projectData);
-    };
-    reader.readAsText(file);
-  };
-  input.click();
-});
-// ===
-
-// === Export
-document.getElementById("btn-export").addEventListener("click", function () {
-	const filename = "exported.html";
-	const htmlContent = editor.getHtml();
-	const cssContent = editor.getCss();
-
-	// Create a temporary div and set its innerHTML to htmlContent
-	const tempDiv = document.createElement("div");
-	tempDiv.innerHTML = htmlContent;
-
-	// Find and remove the first body element
-	const firstBody = tempDiv.querySelector("body");
-	if (firstBody) {
-		firstBody.remove();
-	}
-
-	// Use the updated innerHTML for the exported content
-	const updatedHtmlContent = tempDiv.innerHTML;
-
-	const content = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@pimaonline/pimaonline-themepack/dist/css/themes/cards/styles.css">
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js" defer></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@pimaonline/pimaonline-themepack/dist/js/scripts2.js" defer></script>
-<title>Starter Template</title>
-</head>
-<body>
-${updatedHtmlContent}
-</body>
-</html>
-`;
-	const blob = new Blob([content], { type: "text/html;charset=utf-8" });
-	const link = document.createElement("a");
-	link.href = URL.createObjectURL(blob);
-	link.download = filename;
-	link.click();
-});
-
+const btnExport = document.querySelector("#btn-export");
+btnExport.addEventListener("click", () => exportFile(editor));
