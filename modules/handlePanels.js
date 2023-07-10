7bb8d1629dfcf6ec2741f3087106b87d44a0f0fd
Francisco Reynoso <fjreynoso@pima.edu>
2023-07-10 16:13:26 -0700

    Removed unecessary code for duplicate button

--- modules/handlePanels.js
+++ modules/handlePanels.js
@@ -6,19 +6,6 @@ export function handlePanels(editor) {
     id: "panel-top",
     el: ".panel__top",
   });
-  editor.Panels.addPanel({
-    id: 'layers',
-    el: '.panel__right',
-    // Add the button to the commands array
-    commands: [
-      {
-        id: 'duplicate',
-        className: 'fa fa-clone',
-        attributes: { title: 'Duplicate' },
-        command: 'duplicate-layer',
-      },
-      // ... other buttons
-    ],
-  });
+
   
 }
