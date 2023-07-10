be36770cc68a542f21b34aefb01d49ee7dbab977
Francisco Reynoso <fjreynoso@pima.edu>
2023-07-10 13:09:46 -0700

    Added export function

--- modules/fileOperations.js
+++ modules/fileOperations.js
@@ -1,63 +1,65 @@
-// Function to save data to a local file
-export function saveToLocal(editor) {
-  // Blur the currently focused element
-  if (document.activeElement) {
-    document.activeElement.blur();
-  }
 
-  // Get the editor's data as a JSON string
-  const data = JSON.stringify(editor.getComponents());
 
-  // Create a Blob object representing the data
-  const blob = new Blob([data], {type: 'application/json'});
 
-  // Create a URL for the Blob
-  const url = URL.createObjectURL(blob);
-
-  // Create a link element
-  const link = document.createElement('a');
-  link.href = url;
-  link.download = 'editor-data.json';
-
-  // Append the link to the body
-  document.body.appendChild(link);
-
-  // Simulate a click on the link
-  link.click();
-
-  // Remove the link from the body
-  document.body.removeChild(link);
+export async function saveToLocal(editor) {
+  const storedProjectData = await editor.store();
+  const blob = new Blob([JSON.stringify(storedProjectData)], {type: "application/json;charset=utf-8"});
+  saveAs(blob, "_grapes-project.json");
 }
 
-
-// Function to load data from a local file
-export function loadFromLocal(editor) {
-  // Create an input element
+export function openFromLocal(editor) {
   const input = document.createElement('input');
   input.type = 'file';
   input.accept = 'application/json';
-
-  // Define what happens when a file is selected
-  input.onchange = (event) => {
-    // Get the selected file
-    const file = event.target.files[0];
-
-    // Create a FileReader to read the file
+  input.onchange = async () => {
+    const file = input.files[0];
     const reader = new FileReader();
-
-    // Define what happens when the file has been read
-    reader.onload = (event) => {
-      // Parse the file's content as JSON
-      const components = JSON.parse(event.target.result);
-
-      // Load the components into the editor
-      editor.setComponents(components);
+    reader.onload = async () => {
+      const projectData = JSON.parse(reader.result);
+      await editor.load(projectData);
     };
-
-    // Start reading the file
     reader.readAsText(file);
   };
-
-  // Simulate a click on the input
   input.click();
+}
+
+export function exportFile(editor) {
+	const filename = "exported.html";
+	const htmlContent = editor.getHtml();
+	const cssContent = editor.getCss();
+
+	// Create a temporary div and set its innerHTML to htmlContent
+	const tempDiv = document.createElement("div");
+	tempDiv.innerHTML = htmlContent;
+
+	// Find and remove the first body element
+	const firstBody = tempDiv.querySelector("body");
+	if (firstBody) {
+		firstBody.remove();
+	}
+
+	// Use the updated innerHTML for the exported content
+	const updatedHtmlContent = tempDiv.innerHTML;
+
+	const content = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@pimaonline/pimaonline-themepack/dist/css/themes/cards/styles.css">
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js" defer></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@pimaonline/pimaonline-themepack/dist/js/scripts2.js" defer></script>
+<title>Starter Template</title>
+</head>
+<body>
+${updatedHtmlContent}
+</body>
+</html>
+`;
+	const blob = new Blob([content], { type: "text/html;charset=utf-8" });
+	const link = document.createElement("a");
+	link.href = URL.createObjectURL(blob);
+	link.download = filename;
+	link.click();
 }
