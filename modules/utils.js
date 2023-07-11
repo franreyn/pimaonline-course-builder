222ccde5858e0975068f76c2e468a4aa20c2ef1d
Francisco Reynoso <fjreynoso@pima.edu>
2023-07-11 13:50:35 -0700

    Added types array

--- modules/utils.js
+++ modules/utils.js
@@ -1,4 +1,13 @@
-export function makeComponentsUnselectable(editor, types) {
+//This function takes the components in the array 'types' and prevents mouse selection.
+export function makeComponentsUnselectable(editor) {
+  const types =   [
+    "one-column-layout", 
+    "two-column-layout", 
+    "content-wrapper", 
+    "description-term",
+    "description-definition",
+  ];
+
   editor.on('component:selected', function(component) {
     if (types.includes(component.get('type'))) {
       editor.select(); // Deselect the component
