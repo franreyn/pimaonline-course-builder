dd5f7b0cb295bc40d65aae0029a1b9dae5a3b137
Francisco Reynoso <fjreynoso@pima.edu>
2023-07-13 15:46:07 -0700

    Added config file

---
+++ config.js
@@ -0,0 +1,26 @@
+// myObject.js
+export const config = {
+  copyableComponents: [
+    "border", 
+    "content-body", 
+    "assignment", 
+    "paragraph", 
+    "ol", 
+    "ul", 
+    "dl", 
+    "li", 
+    "dt", 
+    "dd"
+  ],
+  section2: {
+    title: "Main Content",
+    content: "This is the main content section of the document.",
+    images: ["image3.jpg", "image4.jpg"]
+  },
+  section3: {
+    title: "Conclusion",
+    content: "This is the conclusion section of the document.",
+    images: ["image5.jpg", "image6.jpg"]
+  }
+};
+
