e46aa36d2acb40c1787b3f50e34329a38c509344
Francisco Reynoso <fjreynoso@pima.edu>
2023-07-13 16:09:23 -0700

    Added import for config data

--- modules/utils.js
+++ modules/utils.js
@@ -1,17 +1,9 @@
+import { config } from "../config.js";
 // Utils.js is used to house misc functions
 
 // This function takes the components in the array 'types' and prevents mouse selection.
 export function makeComponentsUnselectable(editor) {
-  const types =   [
-    "one-column-layout", 
-    "two-column-layout", 
-    "content-wrapper", 
-    "description-term",
-    "description-definition",
-    "card-body",
-    "card-img",
-    "side-by-side-item"
-  ];
+  const types = config.unselectableComponents;
 
   editor.on('component:selected', function(component) {
     if (types.includes(component.get('type'))) {
