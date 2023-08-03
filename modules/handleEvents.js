3c5c9e690e02e02168f68c262a02ab0464eb1a27
Francisco Reynoso <fjreynoso@pima.edu>
2023-08-03 13:52:26 -0700

    Added logic for active class into confirmation  dialog

--- modules/handleEvents.js
+++ modules/handleEvents.js
@@ -1,4 +1,5 @@
 import { config } from "../config.js";
+import { addComponentToCanvas } from "./utils.js" 
 
 export function handleEvents(editor, layoutsToolbar) {
 
@@ -30,23 +31,34 @@ export function handleEvents(editor, layoutsToolbar) {
   });
   */
 
-	// If you add one type of layout (1 col, 2col, etc) and another is already there, remove the first one but first warn the user
-	let columnComponentCount = 0;
-	editor.on("component:add", (component) => {
-		if (component.get("type") === "one-column-layout" || component.get("type") === "two-column-layout" || component.get("type") === "three-section-layout") {
-			columnComponentCount += 1;
-			if (columnComponentCount > 1) {
-				const confirmSwitch = window.confirm("Your content will be deleted if you switch layouts, are you sure?");
-				if (confirmSwitch) {
-						editor.runCommand("remove-sibling-components", { component });
-				} else {
-					component.remove(); // Remove the component if the user cancels the first confirmation
-				}
-			}
-		}
-		setCustomLayerName(component);
-		editor.LayerManager.render(); // Force layers panel to refresh
-	});
+	// If you add one type of layout component (1 col, 2col, etc) and another is already there, remove the first one but first warn the user. Triggered by `layoutsToolbar.addEventListener("click")`.
+  let columnComponentCount = 0;
+  editor.on("component:add", component => {
+    if (("one-column-layout" === component.get("type") || "two-column-layout" === component.get("type") || "three-section-layout" === component.get("type")) && (columnComponentCount += 1) > 1) {
+      let isConfirmed = window.confirm("Your content will be deleted if you switch layouts, are you sure?");
+      if (isConfirmed) {
+        // User confirmed, so remove sibling components and update active class
+        editor.runCommand("remove-sibling-components", { component: component });
+  
+        // Find the active layout button and remove the "active" class
+        const activeLayoutBtn = layoutsToolbar.querySelector(".layout-btn.active");
+        if (activeLayoutBtn) {
+          activeLayoutBtn.classList.remove("active");
+        }
+  
+        // Find the new layout button and add the "active" class
+        const newLayoutBtn = layoutsToolbar.querySelector(`[data-type="${component.get("type")}"]`);
+        if (newLayoutBtn) {
+          newLayoutBtn.classList.add("active");
+        }
+      } else {
+        // User canceled, so remove the newly added component
+        component.remove();
+      }
+    }
+    setCustomLayerName(component);
+    editor.LayerManager.render();
+  });  
 
 	// When a component is removed force the layers panel to refresh.
 	// The layers panel is a part of the GrapesJS interface that shows a tree view of the components in the editor. By calling render(), the layers panel is updated to reflect the removal of the component.
@@ -74,11 +86,12 @@ export function handleEvents(editor, layoutsToolbar) {
 			}
 		});
 	}
+
 	// Call function to set duplicatable/copyable components. Note: 'allowedCopyableComponents' set in config.js 
 	setCopyableComponents(allowedCopyableComponents);
 	editor.on("component:title", setCustomLayerName);
 
-	// Add custom toolbar for layout components to avoid drag/drop layouts
+  // Listen for click on layout selection buttons
 	layoutsToolbar.addEventListener("click", (event) => {
 		const button = event.target;
 		if (button.tagName === "BUTTON") {
@@ -86,52 +99,4 @@ export function handleEvents(editor, layoutsToolbar) {
 			addComponentToCanvas(editor, componentType);
 		}
 	});
-
-	function addComponentToCanvas(editor, componentType) {		
-  	const existingLayout = editor.DomComponents.getComponents().find(
-    	(component) => component.get("type") === componentType
-  	);
-
-		const layoutsToolbarButtons = layoutsToolbar.querySelectorAll(".layout-btn");
-			layoutsToolbarButtons.forEach((button) => {
-				if (button.classList.contains("active")) {
-					button.classList.remove("active");
-				}
-			}
-		);
-
-		if (!existingLayout) {
-			removeExistingComponent(editor, componentType);
-
-			// Add the component to the editor
-			const addedComponent = editor.DomComponents.addComponent({
-				type: componentType,
-				// Add any default attributes or styles if needed
-			});
-
-			// Select the newly added component
-			editor.select(addedComponent);
-		} else {
-			// If the layout is already active, just select it
-			editor.select(existingLayout);
-		}
-
-		// Mark the button as active
-		const selectedButton = layoutsToolbar.querySelector(`[data-type="${componentType}"]`);
-		selectedButton.classList.add("active");
-	}
-
-	function removeExistingComponent(editor, componentType) {
-		const components = editor.DomComponents.getComponents();
-
-		// Get the type of the component to remove based on the type of the newly added component
-		const typeToRemove = componentType === "one-column-layout" ? "two-column-layout" : componentType === "two-column-layout" ? "three-section-layout" : "one-column-layout";
-
-		// Find the existing component of the opposite type and remove it
-		const existingComponent = components.find((component) => component.get("type") === typeToRemove);
-		if (existingComponent) {
-			editor.select(existingComponent);
-			editor.runCommand("core:component-delete");
-		}
-	}
 }
