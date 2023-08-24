4d0e1d68442eeb03f72b479a393b4e410160bf8b
Clint Mossman <cmossman@pima.edu>
2023-08-24 15:10:18 -0700

    Edit script type to call override js

--- modules/custom-types/addScript.js
+++ modules/custom-types/addScript.js
@@ -3,17 +3,13 @@ export function addScript(editor) {
 		model: {
 			defaults: {
 				tagName: "script",
-				attributes: {
-					type: "text/javascript",
-          src: "https://cdn.jsdelivr.net/npm/@pimaonline/pimaonline-themepack/dist/js/scripts2.js",
-          defer: "true"
-				},
+				type: "text/javascript",
+				src: "../../js/override.js",
+				defer: "true",
 			},
 			init() {
-				if (!this.components().find((component) => component.get("type") === "script")) {
-					this.components().add({ type: "script" });
-				}
-			},
+
+      },
 		},
 	});
 }
