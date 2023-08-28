8191432d8356a8954eb0298939e6680d5f007926
Clint Mossman <cmossman@pima.edu>
2023-08-28 09:18:49 -0700

    Removed ckeditor options from headings

--- modules/custom-types/addHeadings.js
+++ modules/custom-types/addHeadings.js
@@ -1,85 +1,33 @@
 export function addH1(editor) {
   editor.DomComponents.addType("h1", {
-		model: {
-			defaults: {
-				tagName: "h1",
-				toolbar: [], // Empty toolbar
-				// attributes: { contenteditable: "true" },
-				components: [
-					{
-						type: "text",
-						content: "Add subheading",
-					},
-				],
-				rte: {
-					enable(el, rte) {
-						// If already exists just focus
-						if (rte) {
-							rte.focus(); 
-							return rte;
-						}
-						// CKEditor initialization
-						rte = CKEDITOR.inline(el, {
-							// Your configurations...
-							toolbar: [], // Empty toolbar
-							removeButtons: null, // Don't remove any buttons
-							// IMPORTANT
-							sharedSpaces: {
-								top: editor.RichTextEditor.getToolbarEl(),
-							}
-						});
-						return rte;
-					},
-					disable(el, rte) {
-						el.contentEditable = false;
-						rte?.focusManager?.blur(true);
-					},
-				},
-			},
-		},
-	});
+    model: {
+      defaults: {
+        tagName: "h1",
+        components: [
+          {
+            type: "text",
+            content: "Add subheading",
+          },
+        ],
+      },
+    },
+  });
 }
 
 export function addH2(editor) {
   editor.DomComponents.addType("h2", {
-		model: {
-			defaults: {
-				tagName: "h2",
-				toolbar: [], // Empty toolbar
-				// attributes: { contenteditable: "true" },
-				components: [
-					{
-						type: "text",
-						content: "Add subheading",
-					},
-				],
-				rte: {
-					enable(el, rte) {
-						// If already exists just focus
-						if (rte) {
-							rte.focus(); 
-							return rte;
-						}
-						// CKEditor initialization
-						rte = CKEDITOR.inline(el, {
-							// Your configurations...
-							toolbar: [], // Empty toolbar
-							removeButtons: null, // Don't remove any buttons
-							// IMPORTANT
-							sharedSpaces: {
-								top: editor.RichTextEditor.getToolbarEl(),
-							}
-						});
-						return rte;
-					},
-					disable(el, rte) {
-						el.contentEditable = false;
-						rte?.focusManager?.blur(true);
-					},
-				},
-			},
-		},
-	});
+    model: {
+      defaults: {
+        tagName: "h2",
+        components: [
+          {
+            type: "text",
+            content: "Add subheading",
+          },
+        ],
+      },
+    },
+  });
 }
 
 export function addH3(editor) {
