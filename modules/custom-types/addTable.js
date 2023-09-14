e63a8e9666978108f4ef1d979a8063e1eda02bb6
aphan-webdev <aphan@pima.edu>
2023-09-14 13:26:10 -0700

    Add functionality for adding rows and columns

--- modules/custom-types/addTable.js
+++ modules/custom-types/addTable.js
@@ -8,13 +8,55 @@ export function addTable(editor) {
 				},
 			},
 			init() {
-				if (!this.components().find((component) => component.get("type") === "thead")) {
-					this.components().add({ type: "thead" });
-				}
-				if (!this.components().find((component) => component.get("type") === "tbody")) {
-					this.components().add({ type: "tbody" });
-				}
-			},
+				// Create a flag to track whether the view is available
+        this.isViewAvailable = false;
+
+        // Check if the view is available, and if not, wait for it to be ready
+        if (this.view) {
+          this.isViewAvailable = true;
+          this.addButton("+ Add Column", "add-column-btn", () => this.addColumn());
+          this.addButton("+ Add Row", "add-row-btn", () => this.addRow());
+        } else {
+          editor.on("component:update", (model) => {
+            if (!this.isViewAvailable && model.view) {
+              this.isViewAvailable = true;
+              this.addButton("+ Add Column", "add-column-btn", () => this.addColumn());
+              this.addButton("+ Add Row", "add-row-btn", () => this.addRow());
+            }
+          });
+        }
+
+        if (!this.components().find((component) => component.get("type") === "thead")) {
+          this.components().add({ type: "thead" });
+        }
+        if (!this.components().find((component) => component.get("type") === "tbody")) {
+          this.components().add({ type: "tbody" });
+        }
+      },
+      addButton(text, className, clickHandler) {
+        if (this.isViewAvailable) {
+          const button = document.createElement("button");
+          button.textContent = text;
+          button.className = className;
+          button.addEventListener("click", clickHandler);
+          this.view.el.appendChild(button);
+        }
+      },
+      addColumn() {
+        // Add a new <th> to the <thead>
+				const thead = this.components().find((component) => component.get("type") === "thead");
+				const theadTr = thead.components().find((component) => component.get("type") === "thead-tr");
+				theadTr.components().add({ type: "th" });			
+
+        // Add a new <td> to each <tr> in <tbody>
+        const tbody = this.components().find((component) => component.get("type") === "tbody");
+        tbody.components().forEach((tr) => tr.components().add({ type: "td" }));
+      },
+      addRow() {
+        // Add a new <tr> with <td>'s to <tbody>
+        const tbody = this.components().find((component) => component.get("type") === "tbody");
+        tbody.components().add({ type: "tbody-tr" });
+      },
 		},
 	});
 }
