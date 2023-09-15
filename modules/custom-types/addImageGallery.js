e7ee60f4d4be9f651426118eb908bb4493399291
aphan-webdev <aphan@pima.edu>
2023-09-15 13:36:37 -0700

    Update image size

--- modules/custom-types/addImageGallery.js
+++ modules/custom-types/addImageGallery.js
@@ -63,7 +63,7 @@ export function addStaticImage(editor) {
 			defaults: {
 				tagName: "img",
 				attributes: {
-					src: "https://d2l.pima.edu/shared/images/placeholder/400x400.jpg",
+					src: "https://d2l.pima.edu/shared/images/placeholder/900x900.jpg",
 					alt: "Add image alt text",
 				},
 				draggable: false,
