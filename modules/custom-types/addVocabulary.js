30c1d089b819311df2f8ffb1f9112cb2b6795083
aphan-webdev <aphan@pima.edu>
2023-09-18 12:00:42 -0700

    Add vocab btn component

--- modules/custom-types/addVocabulary.js
+++ modules/custom-types/addVocabulary.js
@@ -9,9 +9,43 @@ export function addVocabulary(editor) {
 				if (!this.components().find((component) => component.get("type") === "vocab-wrapper")) {
 					this.components().add({ type: "vocab-wrapper" });
 				}
-			},
+				if (!this.components().find((component) => component.get("type") === "add-vocab-btn")) {
+					this.components().add({ type: "add-vocab-btn" });
+				}
+
+				// Create a flag to track whether the view is available
+        // this.isViewAvailable = false;
+
+        // Check if the view is available, and if not, wait for it to be ready
+      //   if (this.view) {
+      //     this.isViewAvailable = true;
+      //     this.addButton("+ Add Vocab", "add-vocab-btn", () => this.addVocab());
+      //   } else {
+      //     editor.on("component:update", (model) => {
+      //       if (!this.isViewAvailable && model.view) {
+      //         this.isViewAvailable = true;
+      //         this.addButton("+ Add Vocab", "add-vocab-btn", () => this.addVocab());
+      //       }
+      //     });
+      //   }
+			// },
+
+			// addButton(text, className, clickHandler) {
+      //   if (this.isViewAvailable) {
+      //     const button = document.createElement("button");
+      //     button.textContent = text;
+      //     button.className = className;
+      //     button.addEventListener("click", clickHandler);
+      //     this.view.el.appendChild(button);
+      //   }
+      // },
+
+			// addVocab() {
+			// 	this.components().add({ type: "vocab-wrapper" });
+      // },
 		},
-	});
+	},
+});
 }
 
 export function addVocabularyWrapper(editor) {
@@ -59,6 +93,21 @@ export function addVocabularyDefinition(editor) {
 						content: "Add definition",
 					},
 				],
+			},
+		},
+	});
+}
+
+export function addVocabButton(editor) {
+  editor.DomComponents.addType("add-vocab-btn", {
+		model: {
+			defaults: {
+				tagName: "button",
+				attributes: { 
+					class: "add-vocab-btn",
+					type: "button",
+				},
+				content: "+ Add vocab",
 			},
 		},
 	});
